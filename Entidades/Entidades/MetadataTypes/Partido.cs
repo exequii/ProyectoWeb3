bca45a82fe31f5929b127d98607728ff61b86b98
Ezequiel <71356016+exequii@users.noreply.github.com>
2022-10-26 21:49:10 -0300

    Bot funcionando!

--- Entidades/Entidades/MetadataTypes/Partido.cs
+++ Entidades/Entidades/MetadataTypes/Partido.cs
@@ -4,16 +4,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Entidades.Entidades
 {
+    [MetadataType(typeof(PartidoModelMetaData))]
     public partial class Partido
+    {
+        public Partido() { }
+    }
+
+    public class PartidoModelMetaData
     {
         public int IdPartido { get; set; }
 
+        [Required(ErrorMessage = "Debe seleccionar una opcion")]
         public int IdSeleccion1 { get; set; }
 
+        [Required(ErrorMessage = "Debe seleccionar una opcion")]
         public int IdSeleccion2 { get; set; }
 
+        [Required(ErrorMessage = "Debe ingresar una cantidad de goles")]
+        [Range(0,15,ErrorMessage = "La cantidad ingresada no es valida")]
         public int GolesSeleccion1 { get; set; }
 
+        [Required(ErrorMessage = "Debe ingresar una cantidad de goles")]
+        [Range(0, 15, ErrorMessage = "La cantidad ingresada no es valida")]
         public int GolesSeleccion2 { get; set; }
 
         public virtual Seleccion IdSeleccion1Navigation { get; set; } = null!;
---
+++ Entidades/Entidades/MetadataTypes/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades.Entidades
+{
+    [MetadataType(typeof(UsuarioModelMetaData))]
+    public partial class Usuario
+    {
+        public Usuario() { }
+    }
+
+    public class UsuarioModelMetaData
+    {
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "Email es requerido")]
+        [EmailAddress(ErrorMessage = "Formato invalido")]
+        public string Email { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "Contraseña es requerida")]
+        [StringLength(20, ErrorMessage = "La contraseña no puede exceder los 20 caracteres de longitud")]
+        public string Contraseña { get; set; } = null!;
+    }
+
+}
